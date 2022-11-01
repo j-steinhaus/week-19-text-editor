5a6218748ce09595c3ef4571f5942f464908ab23
j-steinhaus <jessicamichele11@gmail.com>
2022-11-01 09:53:13 -0600

    pseduo-coding

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -1,12 +1,15 @@
 import { openDB } from 'idb';
 
 const initdb = async () =>
+//create new database
   openDB('jate', 1, {
+    //add database schema
     upgrade(db) {
       if (db.objectStoreNames.contains('jate')) {
         console.log('jate database already exists');
         return;
       }
+      //object store creation for data
       db.createObjectStore('jate', { keyPath: 'id', autoIncrement: true });
       console.log('jate database created');
     },
--- client/src/js/install.js
+++ client/src/js/install.js
@@ -3,16 +3,20 @@ const butInstall = document.getElementById('buttonInstall');
 // Logic for installing the PWA
 // TODO: Add an event handler to the `beforeinstallprompt` event
 window.addEventListener('beforeinstallprompt', (event) => {
+    //deferring the ven so it can be triggered later
     window.deferredPrompt = event;
+    //removing the hidden class for the install button
     butInstall.classList.toggle('hidden', false);
 });
 
 // TODO: Implement a click event handler on the `butInstall` element
+//click event handler for install button
 butInstall.addEventListener('click', async () => {
     const promptEvent=window.deferredPrompt;
     if(!promptEvent){
         return;
     }
+    //show the install 
     promptEvent.prompt();
     window.deferredPrompt=null;
     butInstall.classList.toggle('hidden', true);
