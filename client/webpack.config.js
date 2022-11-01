09e26bc3180f2c4aafbd5845d9c655d78c5005d7
j-steinhaus <jessicamichele11@gmail.com>
2022-11-01 09:45:38 -0600

    babel to webpack?

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -31,27 +31,40 @@ plugins: [
   }),
 ],
 
-// TODO: Add CSS loaders and babel to webpack.
+  // TODO: Add CSS loaders and babel to webpack.
 
-module.exports = () => {
-  return {
-    mode: 'development',
-    entry: {
-      main: './src/js/index.js',
-      install: './src/js/install.js'
-    },
-    output: {
-      filename: '[name].bundle.js',
-      path: path.resolve(__dirname, 'dist'),
-    },
-    plugins: [
-      
-    ],
+  module.exports = () => {
+    return {
+      mode: 'development',
+      entry: {
+        main: './src/js/index.js',
+        install: './src/js/install.js'
+      },
+      output: {
+        filename: '[name].bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+      },
+      plugins: [
 
-    module: {
-      rules: [
-        
       ],
-    },
+
+      module: {
+        rules: [{
+          test: /\.css$/i,
+          use: ['style-loader', 'css-loader'],
+        }, {
+          test: /\.m?js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env'],
+              plugins: ['@babel/plugin-proposal-object-rest-spread', '@babel/transform-runtime'],
+            }
+          }
+
+        }
+        ],
+      },
+    };
   };
-};
